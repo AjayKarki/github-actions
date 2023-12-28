721c5dd52623004543538bd6ba1d442eb4325ff8
Aman Maharjan <mhrznamn068@gmail.com>
2023-12-28 14:24:15 +0545

    fixed environment vairable

--- actions/deploy/k8s/scripts/config.sh
+++ actions/deploy/k8s/scripts/config.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 
+ENVIRONMENT=$ENVIRONMENT
+PROJECT_NAME=$PROJECT_NAME
+VAULT_TOKEN=$VAULT_TOKEN
+VAULT_SERVER=$VAULT_SERVER
+VAULT_SECRET_PATH=$VAULT_SECRET_PATH
+VAULT_SECRET_COMMON_PATH=$VAULT_SECRET_COMMON_PATH
+GITLAB_TOKEN=$GITLAB_TOKEN
+GITLAB_SERVER=$GITLAB_SERVER
+GITLAB_PROJECT_NUMBER=$GITLAB_PROJECT_NUMBER
+LOCAL_SETTINGS=$LOCAL_SETTINGS
+LOCAL_SETTINGS_NAME=$LOCAL_SETTINGS_NAME
+
 export KUBECONFIG=kubeconfig.yaml
 
 function configmap() {
   LOCAL_SETTINGS_NAME=$1
-  if kubectl get configmaps app-local-settings --namespace=${{env.PROJECT_NAME}} &> /dev/null; then
-    kubectl delete configmap app-local-settings --namespace=${{env.PROJECT_NAME }}
-    kubectl create configmap app-local-settings --from-file=${LOCAL_SETTINGS_NAME} --namespace=${{env.PROJECT_NAME}}
+  if kubectl get configmaps app-local-settings --namespace=${PROJECT_NAME} &> /dev/null; then
+    kubectl delete configmap app-local-settings --namespace=${PROJECT_NAME}
+    kubectl create configmap app-local-settings --from-file=${LOCAL_SETTINGS_NAME} --namespace=${PROJECT_NAME}
   else
-    kubectl create configmap app-local-settings --from-file=${LOCAL_SETTINGS_NAME} --namespace=${{env.PROJECT_NAME}}
+    kubectl create configmap app-local-settings --from-file=${LOCAL_SETTINGS_NAME} --namespace=${PROJECT_NAME}
   fi
 }
 
-if [ "${{ inputs.LOCAL_SETTINGS }}" == "true" ]; then
-  curl -H "PRIVATE-TOKEN: ${{ env.GITLAB_TOKEN }}" "${{ env.GITLAB_SERVER }}/api/v4/projects/${{ env.GITLAB_PROJECT_NUMBER }}/repository/files/${{env.ENVIRONMENT}}%2F${{ env.LOCAL_SETTINGS_NAME }}/raw?ref=main" -o ${{ env.LOCAL_SETTINGS_NAME }}
-  configmap ${{ env.LOCAL_SETTINGS_NAME }}
-elif [ "${{ inputs.LOCAL_SETTINGS }}" == "false" ]; then
+if [ "${LOCAL_SETTINGS}" == "true" ]; then
+  curl -H "PRIVATE-TOKEN: ${GITLAB_TOKEN}" "${GITLAB_SERVER}/api/v4/projects/${GITLAB_PROJECT_NUMBER}/repository/files/${ENVIRONMENT}%2F${LOCAL_SETTINGS_NAME}/raw?ref=main" -o ${LOCAL_SETTINGS_NAME}
+  configmap ${LOCAL_SETTINGS_NAME}
+elif [ "${LOCAL_SETTINGS}" == "false" ]; then
   echo "Dummy Settings" settings.txt
   configmap settings.txt
 fi
 
-curl -H "X-Vault-Token: ${{ inputs.VAULT_TOKEN }}" https://${{ inputs.VAULT_SERVER }}/v1/${{ inputs.VAULT_SECRET_PATH }} | jq -r .data > .env.json
-curl -H "X-Vault-Token: ${{ inputs.VAULT_TOKEN }}" https://${{ inputs.VAULT_SERVER }}/v1/${{ inputs.VAULT_SECRET_COMMON_PATH }} | jq -r .data > .common.env.json
+curl -H "X-Vault-Token: ${VAULT_TOKEN}" https://${VAULT_SERVER}/v1/${VAULT_SECRET_PATH} | jq -r .data > .env.json
+curl -H "X-Vault-Token: ${VAULT_TOKEN}" https://${VAULT_SERVER}/v1/${VAULT_SECRET_COMMON_PATH} | jq -r .data > .common.env.json
 cat .env.json | jq -r 'to_entries[] | "\(.key)=\(.value)"' | base64 > secrets
 cat .common.env.json | jq -r 'to_entries[] | "\(.key)=\(.value)"' | base64 >> secrets
 
-if kubectl get secret app-secret --namespace=${{ env.PROJECT_NAME }} &> /dev/null; then
-  kubectl delete secret app-secret --namespace=${{env.PROJECT_NAME}}
-  kubectl create secret generic app-secret --from-file=secrets --namespace=${{env.PROJECT_NAME}}
+if kubectl get secret app-secret --namespace=${PROJECT_NAME} &> /dev/null; then
+  kubectl delete secret app-secret --namespace=${PROJECT_NAME}
+  kubectl create secret generic app-secret --from-file=secrets --namespace=${PROJECT_NAME}
 else
-  kubectl create secret generic app-secret --from-file=secrets --namespace=${{env.PROJECT_NAME}}
+  kubectl create secret generic app-secret --from-file=secrets --namespace=${PROJECT_NAME}
 fi
