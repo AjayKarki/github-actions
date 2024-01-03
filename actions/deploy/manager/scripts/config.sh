da974edbe5a8f6f052c0cc047111f5abdb25d1ca
aniecesedhai <awitice23@gmail.com>
2024-01-03 18:26:19 +0545

    update ansible path

--- actions/deploy/manager/scripts/config.sh
+++ actions/deploy/manager/scripts/config.sh
@@ -13,4 +13,4 @@ curl -H "X-Vault-Token: ${VAULT_TOKEN}" https://${VAULT_SERVER}/v1/${VAULT_SECRE
 cat .env.json | jq -r 'to_entries[] | "\(.key)=\(.value)"' > .env && cp .env ./app
 
 echo "Generating manager docker compose"
-ansible-playbook -i ansible/inventory ansible/manager.yml
+ansible-playbook -i ../ansible/inventory ../ansible/manager.yml
