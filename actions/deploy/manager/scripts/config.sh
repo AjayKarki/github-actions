f2b1aaa7b970619eeee0bb1d12fa5f594ed799a2
aniecesedhai <awitice23@gmail.com>
2024-01-03 21:34:36 +0545

    update ansible path

--- actions/deploy/manager/scripts/config.sh
+++ actions/deploy/manager/scripts/config.sh
@@ -11,6 +11,3 @@ echo "Extracting variables from Vault..."
 curl -H "X-Vault-Token: ${VAULT_TOKEN}" https://${VAULT_SERVER}/v1/${VAULT_SECRET_PATH} | jq -r .data > .env.json
 curl -H "X-Vault-Token: ${VAULT_TOKEN}" https://${VAULT_SERVER}/v1/${VAULT_SECRET_COMMON_PATH} | jq -r .data >> .env.json
 cat .env.json | jq -r 'to_entries[] | "\(.key)=\(.value)"' > .env && cp .env ./app
-
-echo "Generating manager docker compose"
-ansible-playbook -i ansible/inventory ansible/manager.yml
